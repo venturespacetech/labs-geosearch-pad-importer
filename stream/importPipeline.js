b0afdea6eea4d6eab58e7a68e5bf661267e15fc0
Chris Whong <chris@whong.org>
2018-01-19 01:01:15 -0500

    Add slack webhook when importer starts and finishes

--- stream/importPipeline.js
+++ stream/importPipeline.js
@@ -1,3 +1,14 @@
+var SlackWebhook = require('slack-webhook')
+var os = require('os');
+
+var slack = new SlackWebhook(process.env.SLACK_WEBHOOK_URL, {
+  defaults: {
+    username: 'GeoSearch Import Bot',
+    channel: '#labs-geocoder-api',
+    icon_emoji: ':robot_face:'
+  }
+});
+
 var streams = {};
 
 streams.csvParser = require('./csv_parser').create;
@@ -8,11 +19,17 @@ streams.elasticsearch = require('pelias-dbclient');
 
 // default import pipeline
 streams.import = function(){
-   streams.csvParser()
-     .pipe( streams.docConstructor() )
-     .pipe( streams.adminLookup() )
-     .pipe( streams.dbMapper() )
-     .pipe( streams.elasticsearch() );
+
+  slack.send(`Starting NYCPAD Pelias Import on ${os.hostname()}`)
+  var stream = streams.csvParser()
+    .pipe( streams.docConstructor() )
+    .pipe( streams.adminLookup() )
+    .pipe( streams.dbMapper() )
+    .pipe( streams.elasticsearch() );
+
+  stream.on('finish', function () {
+    slack.send(`NYCPAD Pelias Import on ${os.hostname()} Finished!`)
+  });
 };
 
 module.exports = streams;
