4d36f666a7d3b9ab96cae7efa73123d4cc59ebd5
Chris Whong <chris@whong.org>
2018-01-31 00:22:19 -0500

    remove slack webhooks

--- stream/importPipeline.js
+++ stream/importPipeline.js
@@ -1,15 +1,4 @@
-var SlackWebhook = require('slack-webhook');
-var os = require('os');
-
 require('dotenv').config();
-
-var slack = new SlackWebhook(process.env.SLACK_WEBHOOK_URL, {
-  defaults: {
-    username: 'GeoSearch Import Bot',
-    channel: '#labs-geocoder-api',
-    icon_emoji: ':robot_face:'
-  }
-});
 
 var streams = {};
 
@@ -21,17 +10,11 @@ streams.elasticsearch = require('pelias-dbclient');
 
 // default import pipeline
 streams.import = function(){
-
-  slack.send(`Starting NYCPAD Pelias Import on ${os.hostname()}`);
   var stream = streams.csvParser()
     .pipe( streams.docConstructor() )
     .pipe( streams.adminLookup() )
     .pipe( streams.dbMapper() )
     .pipe( streams.elasticsearch() );
-
-  stream.on('finish', function () {
-    slack.send(`NYCPAD Pelias Import on ${os.hostname()} Finished!`);
-  });
 };
 
 module.exports = streams;
